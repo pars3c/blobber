1dda065702af91b85bc0b110a0346d3cc33ef495
Aderito Xavier <aderito.xavier@daltix.com>
2020-04-04 14:06:24 +0100

    MEDIUM INSERT TEST

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://aderito-xavier.com/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  title: 'ADEV NINJA',
+  subtitle: 'SOFTWARE DEVELOPER | DEVOPS ASPIRANT | ROCKET LEAGUE PLAYER.',
+  copyright: '',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -20,29 +20,33 @@ module.exports = {
       path: '/pages/about'
     },
     {
+      label: 'Resume',
+      path: '/pages/resume'
+    },
+    {
       label: 'Contact me',
       path: '/pages/contacts'
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'Adérito Xavier',
+    photo: '/profile.jpg',
+    bio: 'SOFTWARE DEVELOPER | DEVOPS ASPIRANT | ROCKET LEAGUE PLAYER.',
     contacts: {
-      email: '',
-      facebook: '#',
+      email: 'adev.ninja@gmail.com',
+      facebook: 'https://www.facebook.com/aderito.xavier.35',
       telegram: '#',
       twitter: '#',
-      github: '#',
+      github: 'https://gist.github.com/pars3c',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
+      linkedin: 'https://www.linkedin.com/in/believer-of-the-monolith/',
       instagram: '#',
       line: '',
       gitlab: '',
       weibo: '',
       codepen: '',
-      youtube: '',
+      youtube: 'https://www.youtube.com/channel/UCHrIkwNRqQDHJlUj_6Q34XQ/',
       soundcloud: '',
     }
   }
