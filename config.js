27340cbbf3ca798b908b4789ee8b6b704aea54c4
Aderito Xavier <aderito.xavier@daltix.com>
2020-04-05 21:30:31 +0100

    resume removed

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ module.exports = {
     /*{
       label: 'About me',
       path: '/pages/about'
-    },*/
+    },
     {
       label: 'Resume',
       path: '/pages/resume'
-    },
+    },*/
     {
       label: 'Contact me',
       path: '/pages/contacts'
